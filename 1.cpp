ae5d00af40186408255e8b02e04cd3fa1758251f
Samin Thapa <chhetrisamin442@gmail.com>
2025-08-02 08:50:31 +0545

    1.cpp

---
+++ 1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+class Vehicle2;
+class Vehicle1 
+{
+    private:
+        int price;
+    public:
+        void setPrice(int p) 
+        {
+            price = p;
+        }
+        friend void max(Vehicle1, Vehicle2);
+};
+class Vehicle2 
+{
+    private:
+        int price;
+    public:
+        void setPrice(int p) 
+        {
+            price = p;
+        }
+        friend void max(Vehicle1, Vehicle2);
+};
+void max(Vehicle1 v1, Vehicle2 v2) 
+{
+    if(v1.price > v2.price)
+        cout << "Vehicle1 has higher price: " << v1.price << endl;
+    else if(v2.price > v1.price)
+        cout << "Vehicle2 has higher price: " << v2.price << endl;
+    else
+        cout << "Both vehicles have same price: " << v1.price << endl;
+}
+int main()
+ {
+    Vehicle1 v1;
+    Vehicle2 v2;
+    v1.setPrice(50000);
+    v2.setPrice(60000);
+    max(v1, v2);
+    return 0;
+}
